ec11b6291b4dff08f0db51e37ff5644c639cab84
ciaran <ciaranroche@hotmail.co.uk>
2016-11-20 14:36:47 +0000

    towers of hanoi added

--- RercusiveMethods/Recursion.java
+++ RercusiveMethods/Recursion.java
@@ -3,7 +3,20 @@ public class Recursion {
 
 	public static void main(String [] args){
 		//countDown(3);
-		System.out.println(sumOf(2));
+		//System.out.println(sumOf(3));
+		//int[] array = {1,2,3,4,5,6,7,8,9};
+		//displayArray(array, 1-1, 9-1);
+	}
+
+	private static void displayArray(int array[],int first,int last) {
+		// TODO Auto-generated method stub
+		if(first == last){
+			System.out.println(array[first]+ " ");
+		}else{
+			int mid = first + last/2;
+			displayArray(array, first, mid);
+			displayArray(array, mid+1, last);
+		}
 	}
 
 	private static int sumOf(int n) {
@@ -13,6 +26,7 @@ public class Recursion {
 			sum = 1;
 		}else{
 			sum = sumOf(n-1) + n;
+			return sum;
 		}
 		return sum;
 	}
---
+++ TowersOfHanoi/TowersOfHanoi.java
@@ -0,0 +1,21 @@
+import edu.princeton.cs.introcs.Stopwatch;
+
+public class TowersOfHanoi {
+	public static void move(int numberOfDisks, int startPole, int tempPole, int endPole){
+		if (numberOfDisks > 0){
+			move(numberOfDisks -1, startPole, endPole, tempPole);
+			System.out.println("Move disk " +numberOfDisks + " from pole " + startPole + " to pole " + endPole);
+			move(numberOfDisks - 1, tempPole, startPole, endPole);
+		}
+	}
+	
+	public static void main(String[] args){
+		Stopwatch stopwatch = new Stopwatch();
+		
+		move(20, 1, 2, 3);
+		
+		double time = stopwatch.elapsedTime();
+		System.out.println("elapsed time " + time);
+	}
+
+}
